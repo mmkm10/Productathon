8b3c019aebf1bac09049e0c3afe79a1150abb864
mahir29 <mahirhardwar@gmail.com>
2020-12-28 23:51:10 +0530

    First Commit

--- client/src/App.js
+++ client/src/App.js
@@ -5,21 +5,20 @@ import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import Practice from './components/Practice';
 import Learning from './components/Learning';
-import Writing from './components/Writing';
+import Writing from './components/Writing/Join/Join';
+import Chat from './components/Writing/Chat/Chat'
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-         <Navbar />
-         <Switch>
-           <Route path="/" render={(props)=><Home/>} />
-           <Route path="/learn" render={(props)=><Learning/>}/>
-           <Route path="/practice" render={(props)=><Practice/>}/>
-           <Route path="/write" render={(props)=><Writing/>}/>
-           <Redirect to="/"/>
-         </Switch>
-         <Footer/>
+        <Navbar />
+        <Route path="/write" exact component={Writing} />
+        <Route path="/chat" component={Chat} />
+        <Route exact path="/" render={(props)=><Home/>} />
+        <Route path="/learn" render={(props)=><Learning/>}/>
+        <Route path="/practice" render={(props)=><Practice/>}/>
+        <Footer/>
       </div>
     </BrowserRouter>
 
--- client/src/components/Learning.js
+++ client/src/components/Learning.js
@@ -1,9 +1,33 @@
 import React,{Component} from 'react';
 
+
 class Learning extends Component{
     constructor(props){
         super(props);
     }
+
+    componentDidMount(){
+        fetch("https://google-translate1.p.rapidapi.com/language/translate/v2", {
+	"method": "POST",
+	"headers": {
+		"content-type": "application/x-www-form-urlencoded",
+		"accept-encoding": "application/gzip",
+		"x-rapidapi-key": "01e0eb9d39msh8ea377e9230e0c2p19b6b5jsnc0de51520ee5",
+		"x-rapidapi-host": "google-translate1.p.rapidapi.com"
+	},
+	"body": {
+		"q": "Hello, world!",
+		"source": "en",
+		"target": "es"
+	}
+    })
+    .then(response => {
+        console.log(response);
+    })
+    .catch(err => {
+        console.error(err);
+    });
+        }
 
     render(){
         return(
